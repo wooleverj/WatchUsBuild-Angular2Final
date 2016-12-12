172914f828ce60b927573d5c8b3cee1d9ab1a04b
Woolever <jeremy.woolever@corp.Altegrity.com>
2016-12-12 16:57:33 -0600

    testing

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'racing-app',
+  template: `
+<header class="container">
+  <h1>{{heading}}</h1>
+</header>
+<router-outlet></router-outlet>
+`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  heading = "Ultra Racing Schedule"
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
+
+import { AppComponent } from './app.component';
+import { RacesComponent } from './races/races.component';
+import { RaceService } from './races/race.service';
+import { RaceComponent } from './race/race.component';
+import { RaceDetailComponent } from './race-detail/race-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RacesComponent,
+    RaceComponent,
+    RaceDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'races', pathMatch: 'full'},
+      {path: 'races', component: RacesComponent },
+      {path: 'races/:id', component: RaceDetailComponent}
+      ])
+  ],
+  providers: [ RaceService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
